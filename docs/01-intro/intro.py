a4f3545424c44f944cf7621b59acdb197dfd1899
mirjr <biodasturchi@gmail.com>
2023-04-17 00:17:04 +0500

    01 intro

---
+++ docs/01-intro/intro.py
@@ -0,0 +1,17 @@
+name = input('Enter a file: ')
+handle = open(name, 'r')
+
+counts = dict()
+for line in handle:
+  words = line.split()
+  for word in words:
+    counts[word] = counts.get(word, 0) + 1
+
+bigcount = None
+bigword = None
+for word, count in counts.items():
+  if bigcount is None or count > bigcount:
+    bigword = word
+    bigcount = count
+
+print(bigword, bigcount)
